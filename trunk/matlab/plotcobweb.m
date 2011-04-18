5b4a10e08248b422613d04eed27b345646b9de10
danielelinaro <danielelinaro@ba5a5643-2b51-4d18-95b0-14b3f544da64>
2011-04-18 10:32:49 +0000

    Delete matlab

--- trunk/matlab/plotcobweb.m
+++
@@ -1,8 +0,0 @@
-function plotcobweb(x)
-
-x = x(:)';
-x = repmat(x, [2 1]);
-x = [x(:) x(:)];
-figure; hold on;
-plot(x(2:end-1,1),x(3:end,2),'k')
-plot([min(x(:,1)) max(x(:,1))], [min(x(:,1)) max(x(:,1))], 'Color', [.75 .75 .75], 'LineWidth', 2);
